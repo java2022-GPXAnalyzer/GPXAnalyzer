af754fdece2f08c2268818a530800786cc831fc9
jimmyhealer <yahing6066@gmail.com>
2022-06-22 01:31:38 +0800

    complete colorlist

--- backend/src/test/java/com/fuyajo/GPXAnalayzer/gpx/speedColor/SpeedColorHandlerTest.java
+++ backend/src/test/java/com/fuyajo/GPXAnalayzer/gpx/speedColor/SpeedColorHandlerTest.java
@@ -29,6 +29,13 @@ class SpeedColorHandlerTest {
       .addSegment(segment -> segment
         .addPoint(p -> p.lat(48.20100).lon(16.31651).ele(283))
         .addPoint(p -> p.lat(48.20112).lon(16.31639).ele(278))
+        .addPoint(p -> p.lat(48.20112).lon(16.31639).ele(278))
+        .addPoint(p -> p.lat(48.20112).lon(16.31639).ele(278))
+        .addPoint(p -> p.lat(48.20112).lon(16.31639).ele(278))
+        .addPoint(p -> p.lat(48.20112).lon(16.31639).ele(278))
+        .addPoint(p -> p.lat(48.2012).lon(16.31639).ele(278))
+        .addPoint(p -> p.lat(48.201312).lon(16.315639).ele(278))
+        .addPoint(p -> p.lat(48.202112).lon(16.312639).ele(278))
         .addPoint(p -> p.lat(48.20126).lon(16.31601).ele(274))))
     .build();
     try {
